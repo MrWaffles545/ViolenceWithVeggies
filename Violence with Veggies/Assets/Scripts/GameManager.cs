2fc149edc4f290d0a33d59dc5aa056f5801655fc
MrWaffles545 <abarrwill@gmail.com>
2023-03-09 09:08:14 -0800

    e

--- Violence with Veggies/Assets/Scripts/GameManager.cs
+++ Violence with Veggies/Assets/Scripts/GameManager.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
+    private GameObject box;
+    public int score;
+    private Component controllerScrpt;
+
     // Start is called before the first frame update
     void Start()
     {
+        box = GameObject.Find("SellBox");
+        scoreText.text = "0";
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        score = box.GetComponent<BoxController>().veggieScore;
+        scoreText.text = score.ToString();
     }
 }
