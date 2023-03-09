d753b54a7b9b04af4eee8d588d124265729b9958
Person1246811 <vvvvRussellvvvv@gmail.com>
2023-03-09 10:17:39 -0800

    Game Manager stuff

--- Violence with Veggies/Assets/Scripts/GameManager.cs
+++ Violence with Veggies/Assets/Scripts/GameManager.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText, scoreText2;
+    public TextMeshProUGUI scoreText, scoreText2, gameTimeText;
     private GameObject box;
     public GameObject player, player2;
+    public GameObject endGameUI;
     public int score, score2;
+    public float gameTimer, gameTime;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +25,33 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        score = player.GetComponent<PlayerController>().score;
-        scoreText.text = score.ToString();
-
-        if (player2 != null)
+        if (SceneManager.GetActiveScene().buildIndex != 0)
         {
-            score2 = player2.GetComponent<PlayerController>().score;
-            scoreText2.text = score2.ToString();
+            score = player.GetComponent<PlayerController>().score;
+            scoreText.text = score.ToString();
+
+            if (player2 != null)
+            {
+                score2 = player2.GetComponent<PlayerController>().score;
+                scoreText2.text = score2.ToString();
+            }
+
+            if (gameTimer <= gameTime)
+            {
+                gameTimer += Time.deltaTime;
+                gameTimeText.text = Mathf.RoundToInt(gameTimer).ToString();
+            }
+
+            if (gameTimer >= gameTime)
+            {
+                Time.timeScale = 0;
+                endGameUI.SetActive(true);
+            }
         }
+    }
+
+    public void LoadLevel(int levelNumber)
+    {
+        SceneManager.LoadScene(levelNumber);
     }
 }
