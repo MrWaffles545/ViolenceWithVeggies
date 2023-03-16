e542fdf360c698cbe7e602ef3ef3d913af19e74b
Person1246811 <vvvvRussellvvvv@gmail.com>
2023-03-16 08:55:06 -0700

    wind boundary

--- Violence with Veggies/Assets/Scripts/TileMapController.cs
+++ Violence with Veggies/Assets/Scripts/TileMapController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TileMapController : MonoBehaviour
 {
+    public float range, rangeY, speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,20 @@ public class TileMapController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "item" && collision.transform.parent == null)
+        {
+            Debug.Log("Push");
+            Vector2 temp = collision.GetComponent<Rigidbody2D>().velocity;
+            if (collision.transform.position.x <= -range || collision.transform.position.x >= range)
+                temp.x = 0;
+            if (collision.transform.position.y <= -rangeY || collision.transform.position.y >= rangeY)
+                temp.y = 0;
+            collision.GetComponent<Rigidbody2D>().velocity = temp;
+            collision.transform.position = Vector2.MoveTowards(collision.transform.position, Vector2.zero, speed);
+        }
     }
 }
