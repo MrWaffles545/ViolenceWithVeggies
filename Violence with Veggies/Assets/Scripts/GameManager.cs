6e96cf7b94a1ca67043abfc704103fa31a3dd632
Person1246811 <vvvvRussellvvvv@gmail.com>
2023-03-16 09:47:29 -0700

    More Comments

--- Violence with Veggies/Assets/Scripts/GameManager.cs
+++ Violence with Veggies/Assets/Scripts/GameManager.cs
@@ -7,35 +7,47 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    //UI variables
     public TextMeshProUGUI scoreText, scoreText2, gameTimeText;
     public GameObject player, player2;
     public GameObject endGameUI, pauseUI;
+
+    //Game Variables
     public int score, score2;
     public float gameTime;
+
+    //Weather variables
     public int weather;
     public float weatherTimer, weatherTimerMin, weatherTimerMax;
     public float weatherDuration;
-    public Vector2 wind;
     public bool weatherDone;
+    public Vector2 wind;
+
+    //Pause variable
     public bool paused;
 
     void Update()
     {
+        //Works if it isnt the menu scene
         if (SceneManager.GetActiveScene().buildIndex != 0)
         {
+            //Changes the score UI accordingly
             score = player.GetComponent<PlayerController>().score;
             scoreText.text = score.ToString();
 
+            //Player 2 Score UI code
             if (player2 != null)
             {
                 score2 = player2.GetComponent<PlayerController>().score;
                 scoreText2.text = score2.ToString();
             }
 
+            //Game time code
             if (gameTime >= 0)
             {
                 gameTime -= Time.deltaTime;
                 gameTimeText.text = (Mathf.RoundToInt(gameTime)).ToString();
+                //Pause and resume
                 if (Input.GetKeyDown(KeyCode.Escape))
                 {
                     if (paused)
@@ -45,15 +57,18 @@ public class GameManager : MonoBehaviour
                 }
             }
 
+            //Ends the game if the time runs out
             if (gameTime <= 0)
             {
                 Time.timeScale = 0f;
                 endGameUI.SetActive(true);
             }
 
+            //Weather timer until next weather event
             if (weatherTimer >= 0 && weather == 0)
                 weatherTimer -= Time.deltaTime;
 
+            //When the weather timer runs out it picks a random weather event and random time when the next one starts
             if (weatherTimer <= 0 && weather == 0)
             {
                 weather = Random.Range(1, 5); //1, 2, 3, and 4 (not 5)
@@ -64,15 +79,18 @@ public class GameManager : MonoBehaviour
 
             if (weather == 1) //angry sun
             {
+                //only runs once
                 if (!weatherDone)
                 {
-                    for (int i = 0; i < GameObject.FindGameObjectsWithTag("Soil").Length / 2; i++)
+                    //picks random soil and lights them on fire
+                    for (int i = 0; i < GameObject.FindGameObjectsWithTag("Soil").Length / 4; i++)
                     {
                         SoilController target = GameObject.FindGameObjectsWithTag("Soil")[Random.Range(0, GameObject.FindGameObjectsWithTag("Soil").Length)].GetComponent<SoilController>();
                         target.watered = false;
                         target.fireSpreadDone = false;
                         target.onFire = true;
                     }
+                    //sets the weather duration
                     weatherDuration = 15f;
                     weatherDone = true;
                     Debug.Log("Angry sun");
@@ -81,13 +99,16 @@ public class GameManager : MonoBehaviour
 
             if (weather == 2) //Snow
             {
+                //only runs once
                 if (!weatherDone)
                 {
-                    for (int i = 0; i < GameObject.FindGameObjectsWithTag("Soil").Length / 2; i++)
+                    //picks random soil and covers them with snow
+                    for (int i = 0; i < GameObject.FindGameObjectsWithTag("Soil").Length / 3; i++)
                     {
                         GameObject target = GameObject.FindGameObjectsWithTag("Soil")[Random.Range(0, GameObject.FindGameObjectsWithTag("Soil").Length)];
                         target.GetComponent<SoilController>().stage = -1;
                     }
+                    //sets the weather duration
                     weatherDuration = 15f;
                     weatherDone = true;
                     Debug.Log("Snow");
@@ -96,8 +117,10 @@ public class GameManager : MonoBehaviour
 
             if (weather == 3) //Rain
             {
+                //only runs once
                 if (!weatherDone)
                 {
+                    //waters all the soil or puts out fire then waters after a while
                     for (int i = 0; i < GameObject.FindGameObjectsWithTag("Soil").Length; i++)
                     {
                         SoilController target = GameObject.FindGameObjectsWithTag("Soil")[i].GetComponent<SoilController>();
@@ -109,6 +132,7 @@ public class GameManager : MonoBehaviour
                         else
                             target.watered = true;
                     }
+                    //sets the weather duration
                     weatherDuration = 15f;
                     weatherDone = true;
                     Debug.Log("Rain");
@@ -117,16 +141,20 @@ public class GameManager : MonoBehaviour
 
             if (weather == 4) //Windy
             {
+                //only runs once
                 if (!weatherDone)
                 {
+                    //picks a random direction and magnitude to push the items
                     wind.x = Random.Range(-3, 3);
                     wind.y = Random.Range(-3, 3);
                     for (int i = 0; i < GameObject.FindGameObjectsWithTag("item").Length; i++)
                     {
                         GameObject target = GameObject.FindGameObjectsWithTag("item")[i];
+                        //applies the wind if they arent being held
                         if (target.transform.parent == null)
                             target.GetComponent<Rigidbody2D>().velocity = wind;
                     }
+                    //sets the weather duration
                     weatherDuration = 15f;
                     weatherDone = true;
                     Debug.Log("Windy");
@@ -142,6 +170,7 @@ public class GameManager : MonoBehaviour
 
                 if (weatherDuration <= 0)
                 {
+                    //Turns all the weather stuff off for the items and soil
                     weather = 0;
                     weatherDone = false;
                     wind = Vector2.zero;
--- Violence with Veggies/Assets/Scripts/TileMapController.cs
+++ Violence with Veggies/Assets/Scripts/TileMapController.cs
@@ -10,13 +10,15 @@ public class TileMapController : MonoBehaviour
     {
         if (collision.tag == "item" && collision.transform.parent == null)
         {
-            Debug.Log("Push");
+            //makes temp variable for velocity
             Vector2 temp = collision.GetComponent<Rigidbody2D>().velocity;
+            //if the item is outside the bounds it changes the velocity to zero so it doesnt push that way anymore
             if (collision.transform.position.x <= -range || collision.transform.position.x >= range)
                 temp.x = 0;
             if (collision.transform.position.y <= -rangeY || collision.transform.position.y >= rangeY)
                 temp.y = 0;
             collision.GetComponent<Rigidbody2D>().velocity = temp;
+            //Pushes the item torwards the center if it goes out of bounds
             collision.transform.position = Vector2.MoveTowards(collision.transform.position, Vector2.zero, speed);
         }
     }
